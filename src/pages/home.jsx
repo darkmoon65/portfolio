bdc2e88c4f5e55135a12cd59c830fd206694d85e
Cesar <cesarsantillanaq@gmail.com>
2023-02-27 22:48:13 -0500

    Feature add icons

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,4 @@
+import { AiFillLinkedin,AiFillGithub, AiOutlineSlack } from "react-icons/ai";
 
 export const Home = () => {
     return (
@@ -5,6 +6,12 @@ export const Home = () => {
             <span className="size-normal">¡Hola! Me llamo,</span>
             <span className="size-big">César Santillana</span>
             <span className="size-normal gold">FullStack Developer</span>
+
+            <div>
+                <AiFillLinkedin className="icons" size={50}/>
+                <AiFillGithub className="icons" size={50}/>
+                <AiOutlineSlack className="icons" size={50}/>
+            </div>
         </div>
     )
 }
